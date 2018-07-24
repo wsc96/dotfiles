6be9f68fda9bede874aba44f8e00c4ae85528acb
wsc96 <wesley.campbell@live.com>
2018-07-24 13:08:00 +1000

    commit

--- mplayer-youtube.sh
+++ mplayer-youtube.sh
@@ -1,2 +1,10 @@
-#!/bin/bash
-youtube-dl -q -o- $1 | mplayer -cache 8192  -
+#!/bin/sh
+#
+# Public domain
+# Author: roman [] tsisyk.com
+#
+# Usage: ./me url [youtube-dl parameters]
+#
+
+COOKIE_FILE=/var/tmp/youtube-dl-cookies.txt
+mplayer -cookies -cookies-file ${COOKIE_FILE} $(youtube-dl -g --cookies ${COOKIE_FILE} $*)
